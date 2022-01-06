e2a0e745e8b58b2e52e52dae04ba52c78f6022d4
Your Name <you@example.com>
2022-01-06 11:22:02 -0500

    Subir lo basico de puff

---
+++ puff.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Getting fulan dependency
+ */
+require __DIR__.'/libraries/fulan/helpers.php';
+
+/**
+ * Grouping the other items in a single file
+ */
+
+require __DIR__.'/sistem.php';
+
+
